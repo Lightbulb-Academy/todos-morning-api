bd294786f77d28fd0f8b732b0cd30c10da23eece
Shashank Jha <shashankj677@gmail.com>
2025-02-23 09:44:44 +0545

    WIP authentication

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,27 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class RegisterDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(5)
+  @MaxLength(15)
+  mobile: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
